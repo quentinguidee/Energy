17a60b557c7ad75587af1962521d84fdcb8461bc
Quentin Guidée <quentin.guidee@gmail.com>
2020-09-14 21:53:15 +0200

    [functions] Refactoring

--- functions.py
+++ functions.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 
 import bmesh
 import bpy
@@ -8,81 +7,50 @@ import bpy
 def face_projection_area(face, obj):
     """
     Calculate the z projected surface of a face
-    :param face: TODO
-    :param obj: TODO
-    :return: TODO
     """
     area = 0.0
     transform_matrix = obj.matrix_world
 
     vertices_count = len(face.vertices)
+    vertices = []
+
+    for i in range(vertices_count):
+        vertex_id = face.vertices[i]
+        vertex = obj.data.vertices[vertex_id]
+        transformed_vertex = transform_matrix @ vertex.co
+        transformed_vertex[2] = 0
+        vertices.append(transformed_vertex)
+
     if vertices_count == 4:
-        # Quad
-        # Get vertex indices
-        v1, v2, v3, v4 = face.vertices
+        vector0 = vertices[1] - vertices[0]
+        vector1 = vertices[3] - vertices[0]
 
-        # Get vertex data
-        v1 = obj.data.vertices[v1]
-        v2 = obj.data.vertices[v2]
-        v3 = obj.data.vertices[v3]
-        v4 = obj.data.vertices[v4]
-
-        # Apply transform matrix to vertex coordinates.
-        v1 = transform_matrix @ v1.co
-        v2 = transform_matrix @ v2.co
-        v3 = transform_matrix @ v3.co
-        v4 = transform_matrix @ v4.co
-
-        v1[2] = 0
-        v2[2] = 0
-        v3[2] = 0
-        v4[2] = 0
-
-        vec1 = v2 - v1
-        vec2 = v4 - v1
-
-        n = vec1.cross(vec2)
+        n = vector0.cross(vector1)
 
         area = n.length / 2.0
 
-        vec1 = v4 - v3
-        vec2 = v2 - v3
+        vector0 = vertices[3] - vertices[2]
+        vector1 = vertices[1] - vertices[2]
 
-        n = vec1.cross(vec2)
+        n = vector0.cross(vector1)
 
         area += n.length / 2.0
 
     elif vertices_count == 3:
-        # Triangle
-        # Get vertex indices
-        v1, v2, v3 = face.vertices
+        vector0 = vertices[2] - vertices[1]
+        vector1 = vertices[0] - vertices[1]
 
-        # Get vertex data
-        v1 = obj.data.vertices[v1]
-        v2 = obj.data.vertices[v2]
-        v3 = obj.data.vertices[v3]
-
-        # Apply transform matrix to vertex coordinates.
-        v1 = transform_matrix @ v1.co
-        v2 = transform_matrix @ v2.co
-        v3 = transform_matrix @ v3.co
-
-        v1[2] = 0
-        v2[2] = 0
-        v3[2] = 0
-
-        vec1 = v3 - v2
-        vec2 = v1 - v2
-
-        n = vec1.cross(vec2)
+        n = vector0.cross(vector1)
 
         area = n.length / 2.0
 
     return area
 
 
-# Calculate the volume of a mesh object.
 def object_volume(obj):
+    """
+    Calculate the volume of a mesh object.
+    """
     if obj and obj.type == 'MESH' and obj.data:
         # New volume method for bmesh 2015 corrected 2017
 
@@ -109,16 +77,20 @@ def create_html_file(context, filepath):
     base_html_folder = os.path.expanduser('~/') + '/.blender/ArToKi/html_files'
     shutil.copyfile(temp_file, filepath)
     print(os.path.split(filepath)[0])
+
     if os.path.isdir(os.path.split(filepath)[0] + '/html_files'):
         shutil.rmtree(os.path.split(filepath)[0] + '/html_files')
+
     shutil.copytree(base_html_folder, os.path.split(filepath)[0] + '/html_files')
 
     shutil.copy2(bpy.context.scene.atk_aerial, os.path.split(filepath)[0] + '/html_files/aerial.jpg')
     shutil.copy2(bpy.context.scene.atk_elevation, os.path.split(filepath)[0] + '/html_files/elevation.jpg')
+
     bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.context.scene.render.image_settings.color_mode = 'RGBA'
     bpy.context.scene.render.resolution_x = 1024
     bpy.context.scene.render.resolution_y = 768
+
     # TODO: Re-enable this line
     # bpy.context.scene.render.alpha_mode = 'TRANSPARENT'
 
