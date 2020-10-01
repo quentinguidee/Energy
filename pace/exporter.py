d8f560f10522d28453723d07b712ec25e2d8e8f3
Quentin Guidée <quentin.guidee@gmail.com>
2020-10-01 14:32:23 +0000

    [pace] XML (fix ids)

--- pace/exporter.py
+++ pace/exporter.py
@@ -23,17 +23,81 @@ class Wall:
 
 
 class PaceExporter:
+    # TODO: Auto assignment
+    LAST_ID = 15720
+
     def __init__(self):
+        self.current_id = self.LAST_ID
         self.tree: ElementTree = ET.parse('templates/blank.xml')
         self.root: Element = self.tree.getroot()
 
     def export(self, filename: str):
         self.tree.write(filename)
-        self.fix_ids()
 
-    def fix_ids(self):
-        # Rewrite all ids
-        pass
+    # OLD SYSTEM: Replacing ids in the entire document.
+    # def fix_ids(self, filename):
+    #     """
+    #     Rewrite all ids. This considers that there is only one ID in each line.
+    #     """
+    #     with open(filename) as file:
+    #         lines = file.read().splitlines()
+    #
+    #     with open(filename, 'w') as file:
+    #         for line in lines:
+    #             if 'id=\"' in line:
+    #                 start = line.index('id="')
+    #                 end = line.index('"', start + 4) + 1
+    #                 old_id = line[start:end]
+    #                 new_id = 'id="' + str(self.next_id()) + '"'
+    #                 new_line = line.replace(old_id, new_id)
+    #
+    #                 # if DEBUG:
+    #                 if False:
+    #                     print(old_id + " with " + str(start) + " and " + str(end))
+    #                     print(new_id)
+    #                     print("New line: " + new_line)
+    #
+    #             else:
+    #                 new_line = line
+    #
+    #             file.write(new_line + "\n")
+
+    def fix_ids(self, lines):
+        """
+        Rewrite all ids. This considers that there is only one ID in each line.
+        """
+        for i in range(len(lines)):
+            line = lines[i]
+            if 'id=\"' in line:
+                start = line.index('id="')
+                end = line.index('"', start + 4) + 1
+                old_id = line[start:end]
+                old_id_int = old_id[4:-1]
+                new_id_int = self.next_id()
+                new_id = 'id="' + str(new_id_int) + '"'
+                new_line = line.replace(old_id, new_id)
+
+                if DEBUG:
+                    print(old_id + " with " + str(start) + " and " + str(end))
+                    print(new_id)
+                    print("New line: " + new_line)
+
+                lines[i] = new_line
+
+                for j in range(len(lines)):
+                    line = lines[j]
+                    r = 'reference="' + str(old_id_int) + '"'
+                    if r in line:
+                        new_reference_line = line.replace(str(old_id_int), str(new_id_int))
+
+                        if DEBUG:
+                            print(new_reference_line)
+
+                        lines[j] = new_reference_line
+
+    def next_id(self) -> int:
+        self.current_id += 1
+        return self.current_id
 
     def register_new_wall(self, wall: Wall):
         wall_planes = self.root.find('./building/skin/wallPlanes')
@@ -58,10 +122,14 @@ class PaceExporter:
         root_height.set('class', 'java.math.BigDecimal')
         root_height.text = str(wall.height)
 
-        initial.append(wall_planes_template_root)
+        # Fix ids
+        lines = ET.tostringlist(wall_planes_template_root, encoding='unicode', method='xml')
+        self.fix_ids(lines)
+
+        initial.append(ET.fromstringlist(lines))
 
         if DEBUG:
-            ElementTree(wall_planes).write('debug/walls.xml')
+            print(lines)
 
     def register_new_roof(self, roof: Roof):
         roof_planes = self.root.find("./building/skin/roofPlanes")
@@ -93,7 +161,12 @@ class PaceExporter:
         # AUTO: ProjectionSurface
         # AUTO: Rest
 
-        initial.append(roof_planes_template_root)
+        # Fix ids
+        lines = ET.tostringlist(roof_planes_template_root, encoding='unicode', method='xml')
+        self.fix_ids(lines)
+
+        initial.append(ET.fromstringlist(lines))
 
         if DEBUG:
-            ElementTree(roof_planes).write('debug/roofs.xml')
+            print(ET.fromstringlist(lines).getchildren())
+            print(lines)
